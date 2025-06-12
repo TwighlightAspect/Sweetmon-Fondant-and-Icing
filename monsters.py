f5b97bc92837fb7fbd1ee6221fc17f40179a713a
TwighlightAspect <aamoy013@uottawa.ca>
2025-06-12 16:51:43 -0400

    Improved the abstract classes of monsters.py

--- monsters.py
+++ monsters.py
@@ -1,7 +1,8 @@
 import random as rn
+from abc import ABC,abstractmethod
 
-class Mon:
-    def __init__(self, level=0, name='placeholder', health=0, attack=0, defence=0,speed=0):
+class Mon(ABC):
+    def __init__(self, level=0, name='placeholder', health=0, attack=0, defence=0,speed=0,exp=0):
         self.name = name
         self.nickname = name
 
@@ -18,8 +19,23 @@ class Mon:
         
         self.speed = speed
         
+        self.exp = exp
+        self.req_exp = self.get_req_exp()
         
-
+    def get_req_exp(self):
+        return self.level ** 3
+        
+    def levelup(self,excess_exp):
+        self.level+=1
+        exp = excess_exp
+        self.req_exp = self.get_req_exp()
+        
+    def levelstats(self):
+        self.max_health += rn.randint(0,3)
+        self.attack += rn.randint(0,3)
+        self.defence += rn.randint(0,3)
+        self.speed += rn.randint(0,3)
+    
     def deal_damage(self, other):
 
         other.take_damage(self)
@@ -46,75 +62,32 @@ class Mon:
     def disp_lvl(self):
         return str(self.level)
     
-
+    @abstractmethod
     def attack1(self):
         pass
 
     def __str__(self):
         return self.nickname+"("+self.name+") lvl."+str(self.level)
 
-class JollyRancher(Mon):
-    def __init__(self, level, name="Jolly Rancher", health=rn.randint(18,20), attack=rn.randint(5,7), defence=rn.randint(5,6),speed=rn.randint(12,15)):
-        self.name = name
-        self.nickname = name
-        
-        self.max_health = health
-        self.health = health
 
-        self.attack = attack
-        self.defence = defence
-        
-        self.moves = list()
-        self.mymoves = list()
-        
-        self.level = level
-        
-        self.speed = speed
+
+class JollyRancher(Mon):#fire
+    def __init__(self, level, name="Jolly Rancher", health=19, attack=9, defence=5,speed=13,exp=0):
+        super().__init__(level,name,health,attack,defence,speed,exp)
 
     def attack1(self,other):
         self.deal_damage(other)
 
-class Croissant(Mon):
-    def __init__(self, level, name="Criossant", health=rn.randint(16,18), attack=rn.randint(10,13), defence=rn.randint(2,3),speed=rn.randint(15,17)):
-        self.name = name
-        self.nickname = name
-
-        
-        self.max_health = health
-        self.health = health
-
-        self.attack = attack
-        self.defence = defence
-        
-        self.moves = list()
-        self.mymoves = list()
-        
-        self.level = level
-        
-        self.speed = speed
+class Croissant(Mon):#grass
+    def __init__(self, level, name="Criossant", health=17, attack=11, defence=3,speed=6,exp=0):
+        super().__init__(level,name,health,attack,defence,speed,exp)
 
     def attack1(self,other):
         self.deal_damage(other)
         
-class Milkshake(Mon):
-    def __init__(self, level, name="Milk Shake", health=rn.randint(17,19), attack=rn.randint(9,12), defence=rn.randint(3,5),speed=rn.randint(19,21)):
-        self.name = name
-        self.nickname = name
-
-        
-        self.max_health = health
-        self.health = health
-
-        self.attack = attack
-        self.defence = defence
-        
-        self.moves = list()
-        self.mymoves = list()
-        
-        self.level = level
-        
-        self.speed = speed
-
+class Milkshake(Mon):#water
+    def __init__(self, level, name="Milk Shake", health=18, attack=10, defence=4,speed=20,exp=0):
+        super().__init__(level,name,health,attack,defence,speed,exp)
 
     def attack1(self,other):
         self.deal_damage(other)
